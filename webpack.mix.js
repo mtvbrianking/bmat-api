1677fa066d57b539afb84979278d702b10d34a92
mtvbrianking@gmail.com <mtvbrianking@gmail.com>
2019-05-04 14:34:14 +0300

    Install & setup tailwind.
    Remove; bootstrap, jquery dependencies.

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,13 @@ const mix = require('laravel-mix');
  |
  */
 
+let tailwindcss = require('tailwindcss');
+
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/tailwind.scss', 'public/css/app.css')
+    .options({
+        processCssUrls: false,
+        postCss: [
+            tailwindcss('./tailwind.js')
+        ],
+    });
